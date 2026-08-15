b9eaedd48851af729b8f24b652bd9a836adcdb2b
phanidhar2526git config -- global user.email phanidharmithra2526@gmail.comgit config --global user.name phanidhar2526 <phanidharmithra2526@gmail.com>
2026-08-15 13:23:49 +0530

    Day1Changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Welcome to the Java Track");
     }
 }
